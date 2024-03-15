5b1422edce1c7bb7292542bd281f5ea95ac4b135
Selvakumar Shanmugam <selvapac@gmail.com>
2024-03-15 17:38:28 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UploadFilesComponent } from './components/upload-files/upload-files.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UploadFilesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'file-upload-ui';
+  title = 'ESG Survey Automation';
 }
